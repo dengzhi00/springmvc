16a50a486a478fac28039087167eae7cf8e35758
yuansiqi <501258834@qq.com>
2016-09-14 15:32:24 +0800

    111

---
+++ src/test/java/controller/phone.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class phone {
+
+	String str = "good";
+	char[] ch = { 'a', 'b', 'c' };
+	char c='c';
+	int num = 1;
+	double count = 2.123;
+	List list = new ArrayList();
+	public void change(List list,char c) {
+		/*
+		 * ex.str="test ok"; ex.ch[0]='g'; ex.num=2; ex.count=3.123;
+		 */
+		list.add("1");
+		c='x';
+	}
+
+	
+}
---
+++ src/test/java/controller/test2.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class test2 {
+	public static void main(String args[]) {
+		phone ex = new phone();
+		ex.change(ex.list,ex.c);
+		System.out.println(ex.str + "_  " + ex.num + "_  " + ex.count + "_" + ex.ch);
+		System.out.println(ex.list.toString()+ex.c);
+	}
+
+	
+}
